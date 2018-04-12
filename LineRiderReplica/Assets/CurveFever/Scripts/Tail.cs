1b163fcd1ce8e7cb263504bc5dc89ef6666df0ca
lip ables <phillipables1@gmail.com>
2018-04-12 15:00:55 -0400

    method called if Vector 2 list doesnt exist or last value added and current value are greate enough, add Vector2 input to it, set renderer position count equal to our List, the set last position to method input

--- LineRiderReplica/Assets/CurveFever/Scripts/Tail.cs
+++ LineRiderReplica/Assets/CurveFever/Scripts/Tail.cs
@@ -1,6 +1,34 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 public class Tail : MonoBehaviour {
-    Collider2D collide;
+    LineRenderer rend;
+    List<Vector2> trail;
+    private void Update()
+    {
+        trail = new List<Vector2>();
+    }
 
+    public void tailUpdate(Vector2 playerData)
+    {
+        if(trail != null)
+        {
+            addPoint(playerData);
+        }
+
+        if(Vector2.Distance(trail.Last(), playerData) > .2)
+        {
+            addPoint(playerData);
+        }
+    }
+
+    void addPoint(Vector2 playerPos)
+    {
+        trail.Add(playerPos);
+        rend.positionCount = trail.Count;
+        rend.SetPosition(trail.Count - 1, playerPos);
+
+        
+    }
 }
