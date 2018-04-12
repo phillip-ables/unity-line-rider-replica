cd2f7105be2bb7eb207dff1b1e114feac9ec5de9
lip ables <phillipables1@gmail.com>
2018-04-12 17:15:38 -0400

    Head script-> reference speed rotations speed and horizontal variable, in update get input for raw axis horizontal, in fixed update set the translation in the space self to this

--- LineRiderReplica/Assets/CurveFever/Scripts/Head.cs
+++ LineRiderReplica/Assets/CurveFever/Scripts/Head.cs
@@ -1,16 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Head : MonoBehaviour {
+    public float speed = 0f;
+    public float rotSpeed = 0f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private float horizontal;
+
+    private void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+    }
+    private void FixedUpdate()
+    {
+
+        transform.Translate(Vector2.up* speed * Time.deltaTime, Space.Self);
+    }
+
 }
