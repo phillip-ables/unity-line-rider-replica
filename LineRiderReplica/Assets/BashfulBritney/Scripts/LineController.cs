46425479f61a86c6c73b6b5d2f719f64bd70ec76
lip ables <phillipables1@gmail.com>
2018-04-11 20:05:33 -0400

    if left mouse click instantiate line prefab grab script component from that line

---
+++ LineRiderReplica/Assets/BashfulBritney/Scripts/LineController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineController : MonoBehaviour {
+    public GameObject linePrefab;
+    Line activeLine;
+
+	void Start () {
+    }
+	
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject lineGo = Instantiate(linePrefab);
+            activeLine = linePrefab.GetComponent<Line>();
+
+        }
+    }
+}
