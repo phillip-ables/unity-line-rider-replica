c48f806625a47bd0f5ac550c6829c417c2b2f910
lip ables <phillipables1@gmail.com>
2018-04-11 21:01:22 -0400

    get reference to rigid body, if button down is space then rb bodytype = Dynamic

--- LineRiderReplica/Assets/BashfulBritney/Scripts/Player.cs
+++ LineRiderReplica/Assets/BashfulBritney/Scripts/Player.cs
@@ -3,14 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public Rigidbody2D rb;
 	
-	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (Input.GetButtonDown("Play"))
+        {
+            rb.bodyType = RigidbodyType2D.Dynamic;
+        }
+    }
 }
