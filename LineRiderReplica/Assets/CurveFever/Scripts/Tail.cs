f0c4686908c152be8ac9a110fe6d0c897a552c85
lip ables <phillipables1@gmail.com>
2018-04-12 15:01:57 -0400

    delete and try his way

--- LineRiderReplica/Assets/CurveFever/Scripts/Tail.cs
+++ LineRiderReplica/Assets/CurveFever/Scripts/Tail.cs
@@ -3,32 +3,5 @@ using System.Linq;
 using UnityEngine;
 
 public class Tail : MonoBehaviour {
-    LineRenderer rend;
-    List<Vector2> trail;
-    private void Update()
-    {
-        trail = new List<Vector2>();
-    }
 
-    public void tailUpdate(Vector2 playerData)
-    {
-        if(trail != null)
-        {
-            addPoint(playerData);
-        }
-
-        if(Vector2.Distance(trail.Last(), playerData) > .2)
-        {
-            addPoint(playerData);
-        }
-    }
-
-    void addPoint(Vector2 playerPos)
-    {
-        trail.Add(playerPos);
-        rend.positionCount = trail.Count;
-        rend.SetPosition(trail.Count - 1, playerPos);
-
-        
-    }
 }
