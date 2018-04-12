eec322c458baee5e32792d15c2902b0810a5cf70
lip ables <phillipables1@gmail.com>
2018-04-12 15:25:20 -0400

    Tail script reference LineRenderer and a line List of vector2, on Start GetComponent Renderer and Add transform.position to line

--- LineRiderReplica/Assets/CurveFever/Scripts/Tail.cs
+++ LineRiderReplica/Assets/CurveFever/Scripts/Tail.cs
@@ -3,5 +3,14 @@ using System.Linq;
 using UnityEngine;
 
 public class Tail : MonoBehaviour {
+    LineRenderer rend;
+    List<Vector2> line;
+    public float pointDistance;
+
+    private void Start()
+    {
+        rend = GetComponent<LineRenderer>();
+        line.Add(transform.position);
+    }
 
 }
